b214f0745e741394cb0e3a1e8c9e460b53f9d0fd
suffrajet <suffrajets@hotmail.com>
2018-06-02 20:09:02 +0800

    test

---
+++ micropace-ramp-wechat/src/test/java/com/micropace/ramp/RampTest.java
@@ -0,0 +1,17 @@
+package com.micropace.ramp;
+
+import com.micropace.ramp.base.entity.WxApp;
+import com.micropace.ramp.wechat.RampApplacation;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringBootTest(classes = RampApplacation.class)
+public class RampTest {
+
+}
